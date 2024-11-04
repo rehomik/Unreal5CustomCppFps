bbe6112bf7bddf22c3dc5ef1e26789a67ca624c8
yuno0271 <67936693+yuno0271@users.noreply.github.com>
2024-11-04 15:28:15 +0900

    메타버스 엔진
    총알HUD

---
+++ Source/EngineDic1Cpp/SampleHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SampleHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENGINEDIC1CPP_API ASampleHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
